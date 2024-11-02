3a153fc1a719498196a14bbdba70e23516689ded
RudySeo <gun100491@gmail.com>
2024-11-02 23:17:47 +0900

    Feat: 시큐리티 빈등록

--- auth-service/src/main/java/com/example/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -1,25 +1,39 @@
 package com.example.authservice.config;
 
+import com.example.authservice.utill.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .formLogin(AbstractHttpConfigurer::disable)
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((authorizeRequests) ->
-                authorizeRequests.anyRequest().permitAll()
-                    .requestMatchers("/auth/login").permitAll());
-        return http.build();
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                sessionManagement -> sessionManagement.sessionCreationPolicy(
+                    SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                .requestMatchers("/register").permitAll()
+                .anyRequest().permitAll()
+            )
+            .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 
     @Bean
