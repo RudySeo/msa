7a737a7a4186c4765bdcd11fe9eed3743dca929c
RudySeo <gun100491@gmail.com>
2024-11-02 21:12:54 +0900

    Feat: registerUser 추가

--- auth-service/src/main/java/com/example/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.example.authservice.controller;
 
 import com.example.authservice.dto.LoginRequest;
+import com.example.authservice.dto.RegisterRequest;
 import com.example.authservice.dto.TokenResponse;
+import com.example.authservice.model.User;
 import com.example.authservice.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +18,11 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody RegisterRequest request) {
+        User user = authService.registerUser(request);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
