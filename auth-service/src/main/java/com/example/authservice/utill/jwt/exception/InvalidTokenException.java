c4618f4e9d6633a3356547c81c034611ef0eecbe
RudySeo <gun100491@gmail.com>
2024-11-02 23:18:54 +0900

    Feat: 예외처리 추가

---
+++ auth-service/src/main/java/com/example/authservice/utill/jwt/exception/InvalidTokenException.java
@@ -0,0 +1,8 @@
+package com.example.authservice.utill.jwt.exception;
+
+public class InvalidTokenException extends RuntimeException {
+
+    public InvalidTokenException() {
+        super();
+    }
+}
