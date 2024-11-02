f06cf59c6ba9cf8f46402257821b473dedc9a874
RudySeo <gun100491@gmail.com>
2024-11-02 21:13:06 +0900

    Feat: registerUser 추가

--- auth-service/src/main/java/com/example/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/example/authservice/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.authservice.service;
 
 import com.example.authservice.dto.LoginRequest;
+import com.example.authservice.dto.RegisterRequest;
 import com.example.authservice.model.User;
 import com.example.authservice.repository.UserRepository;
 import com.example.authservice.utill.JWTUtil;
@@ -16,6 +17,19 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JWTUtil jwtUtil;
     private final BCryptPasswordEncoder passwordEncoder;
+
+    public User registerUser(RegisterRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException("Username is already taken");
+        }
+
+        User user = User.builder()
+            .username(request.getUsername())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .build();
+
+        return userRepository.save(user);
+    }
 
     public String login(LoginRequest request) {
         Optional<User> userOpt = userRepository.findByUsername(request.getUsername());
