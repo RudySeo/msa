3bcbf118b63bf1597fc1fe0f93edf336a4549d3c
RudySeo <gun100491@gmail.com>
2024-11-02 20:59:40 +0900

    Feat: AuthController 추가

---
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.dto.LoginRequest;
+import com.example.authservice.dto.TokenResponse;
+import com.example.authservice.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
+        String token = authService.login(request);
+        return ResponseEntity.ok(new TokenResponse(token));
+    }
+}
