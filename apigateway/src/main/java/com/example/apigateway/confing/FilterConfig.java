ca58bb91273c173874a8fd1254c42dc11fe57007
RudySeo <gun100491@gmail.com>
2024-11-06 17:42:11 +0900

    Feat: 라우팅 필터추가

--- apigateway/src/main/java/com/example/apigateway/confing/FilterConfig.java
+++ apigateway/src/main/java/com/example/apigateway/confing/FilterConfig.java
@@ -1,17 +1,24 @@
 package com.example.apigateway.confing;
 
+import com.example.apigateway.filter.AuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class FilterConfig {
+
+    private final AuthenticationFilter authenticationFilter;
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
             .route("auth-service", r -> r.path("/auth/**")
+                .filters(
+                    f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                 .uri("http://localhost:8085")) // auth-service URI 확인
             .build();
     }
