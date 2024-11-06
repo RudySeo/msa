01f5519f9ebc230d15260063da9a2400ee54c50e
RudySeo <gun100491@gmail.com>
2024-11-06 17:43:39 +0900

    Feat: JwtProperties 추가

---
+++ apigateway/src/main/java/com/example/apigateway/Utils/jwt/JwtProperties.java
@@ -0,0 +1,8 @@
+package com.example.apigateway.Utils.jwt;
+
+public class JwtProperties {
+
+    public static final String HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final int TOKEN_PREFIX_LENGTH = 7;
+}
